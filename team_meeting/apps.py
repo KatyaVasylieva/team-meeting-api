cdfd11520a1e25ff41c683cc2a1cbdb92c004e69
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-03-10 20:15:27 +0200

    Create .flake8 file and edit other files according to it

--- team_meeting/apps.py
+++ team_meeting/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class TeamMeetingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'team_meeting'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "team_meeting"
