8d37ba8a96180ad20cca7fa171b47b8a955f6e42
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-03-10 20:46:20 +0200

    Create MeetingRoom model in team_meeting app

--- team_meeting/models.py
+++ team_meeting/models.py
@@ -1,3 +1,17 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class MeetingRoom(models.Model):
+    name = models.CharField(max_length=63)
+    capacity = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
+    has_projector = models.BooleanField(default=False)
+    is_soundproof = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["capacity"]
+
+    def __str__(self):
+        return f"{self.name} ({self.capacity} {'person' if self.capacity==1 else 'people'}" \
+               f"{', projector' if self.has_projector else ''}" \
+               f"{', soundproof' if self.is_soundproof else ''})"
