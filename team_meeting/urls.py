a4cb35cbad64dc3b38037d35faba3336fffb5cce
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-03-11 20:36:19 +0200

    Create endpoint for ProjectViewSet

--- team_meeting/urls.py
+++ team_meeting/urls.py
@@ -3,10 +3,12 @@ from rest_framework import routers
 
 from team_meeting.views import (
     MeetingRoomViewSet,
+    ProjectViewSet,
 )
 
 router = routers.DefaultRouter()
 router.register("meeting_rooms", MeetingRoomViewSet)
+router.register("projects", ProjectViewSet)
 
 urlpatterns = [path("", include(router.urls))]
 
