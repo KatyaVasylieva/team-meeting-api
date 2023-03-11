5b0544ba19dd3f18007c7116f2e31e5e82790ff1
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-03-11 13:28:52 +0200

    Register all models in admin panel, create custom UserAdmin class with email instead of username

--- team_meeting/admin.py
+++ team_meeting/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    MeetingRoom,
+    Project,
+    Team,
+    TypeOfMeeting,
+    Meeting,
+    Booking,
+)
+
+admin.site.register(MeetingRoom)
+admin.site.register(Project)
+admin.site.register(Team)
+admin.site.register(TypeOfMeeting)
+admin.site.register(Meeting)
+admin.site.register(Booking)
