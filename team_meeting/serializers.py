42be0459ea16f15620263ab61f0dd35bc202cc81
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-03-11 20:35:30 +0200

    Create ProjectSerializer and ProjectRetrieveSerializer

--- team_meeting/serializers.py
+++ team_meeting/serializers.py
@@ -1,9 +1,24 @@
 from rest_framework import serializers
 
-from team_meeting.models import MeetingRoom
+from team_meeting.models import MeetingRoom, Project
 
 
 class MeetingRoomSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = MeetingRoom
         fields = "__all__"
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Project
+        fields = ("name", "description")
+
+
+class ProjectRetrieveSerializer(ProjectSerializer):
+
+    class Meta:
+        model = Project
+        fields = ("name", "description", "image")
