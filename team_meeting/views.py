c94a94693a67f052e0eec8b90d18dcb69d33b0c3
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-03-11 20:35:58 +0200

    Create ProjectViewSet

--- team_meeting/views.py
+++ team_meeting/views.py
@@ -1,15 +1,34 @@
 from rest_framework import mixins
 from rest_framework.viewsets import GenericViewSet
 
-from team_meeting.models import MeetingRoom
-from team_meeting.serializers import MeetingRoomSerializer
+from team_meeting.models import MeetingRoom, Project
+from team_meeting.serializers import MeetingRoomSerializer, ProjectSerializer, ProjectRetrieveSerializer
 
 
 class MeetingRoomViewSet(
-    mixins.CreateModelMixin,
     mixins.ListModelMixin,
+    mixins.CreateModelMixin,
     GenericViewSet,
 ):
     queryset = MeetingRoom.objects.all()
     serializer_class = MeetingRoomSerializer
     # permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+
+class ProjectViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    GenericViewSet,
+):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ProjectSerializer
+
+        if self.action == "retrieve":
+            return ProjectRetrieveSerializer
+
+        return ProjectSerializer
