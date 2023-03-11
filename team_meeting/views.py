766ce1c05724b029e9b6a7229bcd430f3cd4a9fb
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-03-11 20:18:58 +0200

    Create MeetingRoomViewSet with only list and create actions available

--- team_meeting/views.py
+++ team_meeting/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from team_meeting.models import MeetingRoom
+from team_meeting.serializers import MeetingRoomSerializer
+
+
+class MeetingRoomViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = MeetingRoom.objects.all()
+    serializer_class = MeetingRoomSerializer
+    # permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
