362a49cf61266c7d53e72bae94fb8273b84116dd
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-03-13 12:54:39 +0200

    Create tests for filtering booking api

--- team_meeting/tests/test_booking_api.py
+++ team_meeting/tests/test_booking_api.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
@@ -6,10 +7,11 @@ from rest_framework import status
 from rest_framework.test import APIClient
 
 from team_meeting.models import MeetingRoom, Project, Team, TypeOfMeeting, Meeting, Booking
+from team_meeting.serializers import BookingListSerializer
 
 BOOKING_URL = reverse("team-meeting:booking-list")
-#
-#
+
+
 def sample_room(**params):
     defaults = {
         "name": "Blue",
@@ -29,9 +31,7 @@ def sample_project(**params):
     return Project.objects.create(**defaults)
 
 
-def sample_team(**params):
-    project = sample_project()
-
+def sample_team(project, **params):
     defaults = {
         "name": "Backend",
         "project": project,
@@ -42,10 +42,7 @@ def sample_team(**params):
     return Team.objects.create(**defaults)
 
 
-def sample_meeting(**params):
-    team = sample_team()
-    type_of_meeting = TypeOfMeeting.objects.create("WEEKLY")
-
+def sample_meeting(team, type_of_meeting, **params):
     defaults = {
         "team": team,
         "type_of_meeting": type_of_meeting,
@@ -56,16 +53,13 @@ def sample_meeting(**params):
     return Meeting.objects.create(**defaults)
 
 
-def sample_booking(**params):
-    room = sample_room()
-    meeting = sample_meeting()
-
+def sample_booking(room, meeting, user, **params):
     defaults = {
         "room": room,
         "day": "2023-01-01",
         "start_hour": 10,
         "end_hour": 12,
-        "user": params["user"],
+        "user": user,
         "meeting": meeting,
     }
     defaults.update(params)
@@ -80,3 +74,78 @@ class UnauthenticatedBookingApiTests(TestCase):
     def test_auth_required(self):
         res = self.client.get(BOOKING_URL)
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class AuthenticatedBookingApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            "test@test.com",
+            "testpassword",
+        )
+        self.client.force_authenticate(self.user)
+
+        self.room_blue = sample_room()
+        self.room_yellow = sample_room(name="Yellow")
+
+        self.project_taxi = sample_project()
+        self.project_library = sample_project(name="Library")
+
+        self.team_taxi_back = sample_team(self.project_taxi)
+        self.team_library_back = sample_team(self.project_library)
+
+        self.weekly_type = TypeOfMeeting.objects.create(name="Weekly")
+        self.urgent_type = TypeOfMeeting.objects.create(name="Urgent")
+
+        self.meeting_weekly = sample_meeting(self.team_taxi_back, self.weekly_type)
+        self.meeting_urgent = sample_meeting(self.team_library_back, self.urgent_type)
+
+        self.booking_weekly = sample_booking(self.room_blue, self.meeting_weekly, self.user)
+        self.booking_urgent = sample_booking(self.room_yellow, self.meeting_urgent, self.user, day="2023-01-02")
+
+    def test_list_bookings(self):
+
+        res = self.client.get(BOOKING_URL)
+
+        bookings = Booking.objects.order_by("day", "start_hour")
+        serializer = BookingListSerializer(bookings, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_filter_bookings_by_day(self):
+
+        res = self.client.get(
+            BOOKING_URL, {"day": "2023-01-02"}
+        )
+
+        serializer_w = BookingListSerializer(self.booking_weekly)
+        serializer_u = BookingListSerializer(self.booking_urgent)
+
+        self.assertIn(serializer_u.data, res.data)
+        self.assertNotIn(serializer_w.data, res.data)
+
+    def test_filter_bookings_by_room(self):
+
+        res = self.client.get(
+            BOOKING_URL, {"room": "b"}
+        )
+
+        serializer_w = BookingListSerializer(self.booking_weekly)
+        serializer_u = BookingListSerializer(self.booking_urgent)
+
+        self.assertIn(serializer_w.data, res.data)
+        self.assertNotIn(serializer_u.data, res.data)
+
+    def test_filter_bookings_by_project(self):
+
+        res = self.client.get(
+            BOOKING_URL, {"project": "lib"}
+        )
+
+        serializer_w = BookingListSerializer(self.booking_weekly)
+        serializer_u = BookingListSerializer(self.booking_urgent)
+
+        self.assertIn(serializer_u.data, res.data)
+        self.assertNotIn(serializer_w.data, res.data)
+
